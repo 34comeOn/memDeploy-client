1715b776be211f9699c3eee80db649a9fc026706
Mikhail <barabanovm87@gmail.com>
2024-01-11 15:18:26 +0300

    restore process.env

--- src/RTKApi/rootApi.ts
+++ src/RTKApi/rootApi.ts
@@ -2,6 +2,6 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/dist/query/react';
 
 export const rootAPI = createApi({
     reducerPath: 'rootAPI',
-    baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
+    baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL  }),
     endpoints: () => ({})
   });
