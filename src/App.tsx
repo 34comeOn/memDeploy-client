12e25bf1f09c131aabd36f4bf053eabe76b9b769
Mikhail Barabanov <barabanovm87@gmail.com>
2025-08-05 15:44:38 +0200

    adds scroll toTop

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,22 @@ import './App.scss';
 import { Outlet } from 'react-router-dom';
 import { Header } from './components/organizms/header';
 import { EditModalWindow } from './components/organizms/editModal';
+import { useLocation } from 'react-router-dom';
+
+function ScrollToTop() {
+    const { pathname } = useLocation();
+  
+    React.useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
+  
+    return null;
+}
+  
 
 const App = () =>
 <>
+    <ScrollToTop />
     <Header />
     <div className='App--container__page'>
         <Outlet />
