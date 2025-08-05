501eae1647a62960851677e8fd1fbd954e74f987
Mikhail Barabanov <barabanovm87@gmail.com>
2025-08-05 11:32:58 +0200

    restores rootAPI

--- src/RTKApi/rootApi.ts
+++ src/RTKApi/rootApi.ts
@@ -2,6 +2,6 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/dist/query/react';
 
 export const rootAPI = createApi({
     reducerPath: 'rootAPI',
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3005'}),
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://memorizer-app.com:3002'}),
     endpoints: () => ({})
   });
